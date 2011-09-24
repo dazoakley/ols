0cbe89246766fcae5eef7b3e3c8b4cbd31e8b2c5
Darren Oakley <daz.oakley@gmail.com>
2011-09-24 09:28:18 +0100

    Laying in some basic metadata and a generic request handler

--- lib/ols.rb
+++ lib/ols.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
 
 require 'savon'
+require 'ap'
 
 # Simple wrapper for interacting with the OLS (Ontology Lookup Service - http://www.ebi.ac.uk/ontology-lookup/) 
 # database (created and managed by the EBI).
@@ -9,5 +11,39 @@ module OLS
   # Error class for when we can't find a given ontology term.
   class OntologyTermNotFoundError < StandardError; end
 
+  class << self
+    def client
+      @client = setup_soap_client if @client.nil?
+      @client
+    end
+
+    def version
+      request :get_version
+    end
+
+    def ontologies
+      @ontologies ||= {}
+      if @ontologies.empty?
+        response = request :get_ontology_names
+        response[:item].each do |ont|
+          @ontologies[ont[:key]] = ont[:value]
+        end
+      end
+      @ontologies
+    end
+
+    private
+
+    def setup_soap_client
+      Savon::Client.new do
+        wsdl.document = "http://www.ebi.ac.uk/ontology-lookup/OntologyQuery.wsdl"
+      end
+    end
+
+    def request(method)
+      response = self.client.request method
+      response.body[:"#{method}_response"][:"#{method}_return"]
+    end
+  end
 
 end
--- ols.gemspec
+++ ols.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "shoulda"
   s.add_development_dependency "simplecov"
   s.add_development_dependency "simplecov-rcov"
+  s.add_development_dependency "awesome_print"
 end
