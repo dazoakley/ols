58d7acb07a378921d20f12fd6d2f2eb97720f13a
Darren Oakley <daz.oakley@gmail.com>
2011-10-21 11:02:49 +0100

    Added an MP term with two direct parents into the test suite to make sure we test all aspects of the multiple parent handling.

--- test/test_ols_term.rb
+++ test/test_ols_term.rb
@@ -12,26 +12,30 @@ class OLSTermTest < Test::Unit::TestCase
 
     context 'once created' do
       setup do
-        @term = OLS.find_by_id('EMAP:3018')
+        @emap_term = OLS.find_by_id('EMAP:3018')
+        @mp_term   = OLS.find_by_id('MP:0002115')
       end
 
       should 'have an id/term and a name' do
-        assert_equal 'EMAP:3018', @term.term_id
-        assert_equal 'TS18,nose', @term.term_name
+        assert_equal 'EMAP:3018', @emap_term.term_id
+        assert_equal 'TS18,nose', @emap_term.term_name
+        assert_equal 'MP:0002115', @mp_term.term_id
+        assert_equal 'abnormal skeleton extremities morphology', @mp_term.term_name
       end
 
       should 'be able to represent itself as a string' do
-        assert_equal 'EMAP:3018 - TS18,nose', @term.to_s
+        assert_equal 'EMAP:3018 - TS18,nose', @emap_term.to_s
       end
 
       should 'be able to say if it is an obsolete term' do
-        assert_equal false, @term.is_obsolete?
+        assert_equal false, @emap_term.is_obsolete?
         assert_equal false, OLS.find_by_id('GO:0007242').is_obsolete?
         assert OLS.find_by_id('GO:0008945').is_obsolete?
       end
 
       should 'be able to give its definition' do
-        assert_nil @term.definition
+        assert_nil @emap_term.definition
+        assert_equal 'any structural anomaly of the limb, autopod or tail bones', @mp_term.definition
 
         go_term = OLS.find_by_id('GO:0007242')
         assert_equal 'intracellular signal transduction', go_term.term_name
@@ -59,64 +63,77 @@ class OLSTermTest < Test::Unit::TestCase
 
         assert go_term.synonyms.keys.include? :alt_id
         assert go_term.synonyms[:alt_id].include? 'GO:0023013'
+
+        assert_equal Hash, @emap_term.synonyms.class
+        assert @emap_term.synonyms.empty?
       end
 
       should 'be able to report its parents' do
-        assert_equal 1, @term.parents.size
-        assert_equal 'EMAP:2987', @term.parents.first.term_id
-        
-        # TODO: find a term with more than 1 parent!!!
+        assert_equal 1, @emap_term.parents.size
+        assert_equal 'EMAP:2987', @emap_term.parents.first.term_id
+
+        mp_term_parent_term_ids = @mp_term.parents.map(&:term_id)
+
+        assert_equal 2, @mp_term.parents.size
+        assert mp_term_parent_term_ids.include?('MP:0000545')
+        assert mp_term_parent_term_ids.include?('MP:0009250')
       end
 
       should 'be able to generate a flat list of ALL parents (up the ontology)' do
-        assert @term.all_parents.is_a? Array
-        assert @term.all_parents.first.is_a? OLS::Term
-        assert_equal 4, @term.all_parents.size
-        assert_equal 'EMAP:0', @term.all_parents.first.term_id
+        assert @emap_term.all_parents.is_a? Array
+        assert @emap_term.all_parents.first.is_a? OLS::Term
+        assert_equal 4, @emap_term.all_parents.size
+        assert_equal 'EMAP:0', @emap_term.all_parents.first.term_id
+
+        assert @mp_term.all_parents.is_a? Array
+        assert @mp_term.all_parents.first.is_a? OLS::Term
+        assert_equal 6, @mp_term.all_parents.size
+        assert_equal 'MP:0000001', @mp_term.all_parents.first.term_id
+        assert @mp_term.all_parents.last.term_id =~ /MP:0000545|MP:0009250/
       end
 
       should 'be able to generate a flat list of ALL parent terms/names' do
-        assert @term.all_parent_ids.is_a? Array
-        assert_equal 4, @term.all_parent_ids.size
-        assert_equal 'EMAP:0', @term.all_parent_ids.first
+        assert @emap_term.all_parent_ids.is_a? Array
+        assert_equal 4, @emap_term.all_parent_ids.size
+        assert_equal 'EMAP:0', @emap_term.all_parent_ids.first
 
-        assert @term.all_parent_names.is_a? Array
-        assert_equal 4, @term.all_parent_names.size
-        assert_equal 'Mouse_anatomy_by_time_xproduct', @term.all_parent_names.first
+        assert @emap_term.all_parent_names.is_a? Array
+        assert_equal 4, @emap_term.all_parent_names.size
+        assert_equal 'Mouse_anatomy_by_time_xproduct', @emap_term.all_parent_names.first
       end
 
       should 'be able to report its children' do
-        assert_equal 3, @term.children.size
+        assert_equal 3, @emap_term.children.size
         emap3022_included = false
-        @term.children.each do |child|
+        @emap_term.children.each do |child|
           emap3022_included = true if child.term_id.eql?('EMAP:3022')
         end
         assert emap3022_included
       end
 
       should 'be able to generate a flat list of ALL children (down the ontology)' do
-        assert @term.all_children.is_a? Array
-        assert @term.all_children.first.is_a? OLS::Term
-        assert_equal 14, @term.all_children.size
+        assert @emap_term.all_children.is_a? Array
+        assert @emap_term.all_children.first.is_a? OLS::Term
+        assert_equal 14, @emap_term.all_children.size
         emap3032_included = false
-        @term.all_children.each do |child|
+        @emap_term.all_children.each do |child|
           emap3032_included = true if child.term_id.eql?('EMAP:3032')
         end
       end
 
       should 'be able to generate a flat list of ALL child terms/names' do
-        assert @term.all_child_ids.is_a? Array
-        assert_equal 14, @term.all_child_ids.size
-        assert @term.all_child_ids.include?('EMAP:3032')
+        assert @emap_term.all_child_ids.is_a? Array
+        assert_equal 14, @emap_term.all_child_ids.size
+        assert @emap_term.all_child_ids.include?('EMAP:3032')
 
-        assert @term.all_child_names.is_a? Array
-        assert_equal 14, @term.all_child_names.size
-        assert @term.all_child_names.include?('TS18,mesenchyme,medial-nasal process')
+        assert @emap_term.all_child_names.is_a? Array
+        assert_equal 14, @emap_term.all_child_names.size
+        assert @emap_term.all_child_names.include?('TS18,mesenchyme,medial-nasal process')
       end
 
       should "be able to say if it's a root/leaf node" do
-        assert_equal false, @term.is_root?
-        assert_equal false, @term.is_leaf?
+        assert_equal false, @emap_term.is_root?
+        assert_equal false, @emap_term.is_leaf?
 
         emap3032 = OLS.find_by_id('EMAP:3032')
         assert_equal false, emap3032.is_root?
